49c9668b8efd706297613cd05632a82acb487d24
tuayang <tuayang@gmail.com>
2019-06-01 14:29:59 -0700

    initial updated folders

---
+++ src/components/projects/BlogList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const BlogList = () => {
+    return (
+        <div className="blog-list section">
+            <div className="card z-depth-0 post-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Post Title</span>
+                    <p>Posted by Master T</p>
+                    <p className="grey-text">1 June, 2pm</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 post-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Post Title</span>
+                    <p>Posted by Master T</p>
+                    <p className="grey-text">1 June, 2pm</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 post-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Post Title</span>
+                    <p>Posted by Master T</p>
+                    <p className="grey-text">1 June, 2pm</p>
+                </div>
+            </div>
+            <div className="card z-depth-0 post-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Post Title</span>
+                    <p>Posted by Master T</p>
+                    <p className="grey-text">1 June, 2pm</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BlogList
